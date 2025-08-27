1f3a37b00d0e2c489dc5ae90c901c436239901d3
Shubham shree <shubhamshree@C-BL-VL-LT-0411.local>
2025-08-27 20:12:41 +0530

    Form Validation, Handle Submit Form

--- components/LoginPage/Login.tsx
+++ components/LoginPage/Login.tsx
@@ -1,17 +1,22 @@
+'use client'
 import Link from 'next/link'
 import LoginForm from './LoginForm'
 import AcUnitIcon from '@mui/icons-material/AcUnit'
 
 const Login = () => {
   return (
-    <div className="grid grid-cols-2 h-screen">
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen">
+      {/* Left Panel */}
       <div className="bg-blue-500 flex items-center justify-center">
         <h1 className="text-white text-4xl font-bold">Welcome Back!</h1>
       </div>
-      <div className="flex flex-col items-center gap-7 py-10">
-        <div className="mt-auto flex flex-col items-center gap-3 w-3/5">
+
+      {/* Right Panel - Form */}
+      <div className="flex flex-col items-center gap-7 py-10 px-3">
+        {/* Header */}
+        <div className="mt-auto flex flex-col items-center gap-3 w-full sm:w-3/5">
           <div className="p-3 bg-gray-50 border border-gray-100 rounded-full">
-            <AcUnitIcon fontSize="large" className=" text-blue-500" />
+            <AcUnitIcon fontSize="large" className="text-blue-500" />
           </div>
           <h2 className="text-2xl">Hello Again!</h2>
           <p className="text-gray-400 text-center">
@@ -19,9 +24,13 @@ const Login = () => {
             facilisis libero erat eget nisl
           </p>
         </div>
+
+        {/* Form */}
         <LoginForm />
+
+        {/* Footer */}
         <div className="flex items-center gap-2 mt-auto">
-          <p>Don't have account yet?</p>
+          <p>Don&apos;t have account yet?</p>
           <Link href="/register" className="text-blue-500 font-semibold">
             Sign Up
           </Link>
--- components/LoginPage/LoginForm.tsx
+++ components/LoginPage/LoginForm.tsx
@@ -9,9 +9,19 @@ import IconButton from '@mui/material/IconButton'
 import LockOpenIcon from '@mui/icons-material/LockOpen'
 import LockIcon from '@mui/icons-material/Lock'
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail'
+import Link from 'next/link'
+import axios from 'axios'
 
 const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const [error, setError] = useState({
+    email: false,
+    password: false,
+  })
 
   const handleClickShowPassword = () => setShowPassword((show) => !show)
 
@@ -27,12 +37,79 @@ const LoginForm = () => {
     event.preventDefault()
   }
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    setFormData({ ...formData, [name]: value })
+
+    if (value !== '') {
+      setError((prev) => ({ ...prev, [name]: false }))
+    }
+  }
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    if (value.trim() === '') {
+      setError((prev) => ({ ...prev, [name]: true }))
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    let newErrors = {
+      email: formData.email === '' || !emailRegex.test(formData.email),
+      password: formData.password === '',
+    }
+
+    setError(newErrors)
+
+    if (!newErrors.email && !newErrors.password) {
+      try {
+        //console.log('Form submitted:', formData)
+        const url = 'http://localhost:8001/api/auth/login'
+
+        const response = await axios.post(url, formData)
+
+        //console.log('Login Successful:', response.data)
+
+        localStorage.setItem('authToken', response.data.accessToken)
+
+        setFormData({
+          email: '',
+          password: '',
+        })
+      } catch (error: any) {
+        if (error.response) {
+          console.error(
+            'Login failed:',
+            error.response.data.message || error.response.data
+          )
+        } else {
+          console.error('Error:', error.message)
+        }
+      }
+    }
+  }
+
   return (
-    <form className="w-3/5 flex flex-col">
-      <FormControl fullWidth sx={{ marginBottom: 3 }} variant="outlined">
+    <form className="w-full sm:w-3/5 flex flex-col" onSubmit={handleSubmit}>
+      <FormControl
+        fullWidth
+        sx={{ marginBottom: 3 }}
+        variant="outlined"
+        error={error.email}
+      >
         <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
         <OutlinedInput
           id="outlined-adornment-email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
           endAdornment={
             <InputAdornment position="end">
               <AlternateEmailIcon />
@@ -42,10 +119,14 @@ const LoginForm = () => {
         />
       </FormControl>
 
-      <FormControl fullWidth variant="outlined">
+      <FormControl fullWidth variant="outlined" error={error.password}>
         <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
         <OutlinedInput
           id="outlined-adornment-password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
           type={showPassword ? 'text' : 'password'}
           endAdornment={
             <InputAdornment position="end">
@@ -66,20 +147,30 @@ const LoginForm = () => {
         />
       </FormControl>
 
-      <div className="flex items-center justify-between mt-3">
-        <div className="flex items-center gap-2">
-          <input type="checkbox" id="rememberMe" />
+      {/* Remember Me & Recovery Password */}
+      <div className="flex items-center justify-end mt-3">
+        {/* <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            checked={rememberMe}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setRememberMe(e.target.checked)
+            }
+          />
           <label htmlFor="rememberMe" className="text-sm">
             Remember Me
           </label>
-        </div>
-        <a
+        </div> */}
+        <Link
           className="inline-block align-baseline font-semibold text-sm text-blue-500 hover:text-blue-800"
           href="#"
         >
           Recovery Password
-        </a>
+        </Link>
       </div>
+
+      {/* Login Button */}
       <button
         className="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         type="submit"
